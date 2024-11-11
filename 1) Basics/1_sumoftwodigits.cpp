11b7e74b1fca4e1e1f4b09416737fbf4d5e760c4
Prince Ojha <princeojha014@gmail.com>
2024-11-11 15:08:55 +0530

    first commit

---
+++ 1) Basics/1_sumoftwodigits.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter 1st Number :";
+    cin>>a;
+    cout<<"Enter 2nd Number :";
+    cin>>b;
+    sum=a+b;
+    cout<<"Sum of two digits are : "<<a<<" + "<<b<<" ="<< sum;
+    return 0;
+}
